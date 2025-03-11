002c13380e4bd88865f3d688a28cc8f9366b6edc
Asjad Gull <148690534+Asjadgul@users.noreply.github.com>
2025-03-11 09:32:09 +0000

    infer in TS

---
+++ Inferring_in_TypeScript.ts
@@ -0,0 +1,15 @@
+// Type inference is the process by which TypeScript 
+// automatically determines the type of a  function,
+//  or expression based on its value or context
+
+
+// Inferring of string
+let tech = "TypeScript"
+
+// Inferring of No.
+let no = 99
+
+
+// Inferring of Bool
+let bool = false
+
