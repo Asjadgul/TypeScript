b3c413604275450a15769dea67e60f8262a77cd3
Asjad Gull <148690534+Asjadgul@users.noreply.github.com>
2025-03-11 09:33:05 +0000

    Arrays in TS

---
+++ Arrays_in_TypeScript.ts
@@ -0,0 +1,34 @@
+const nums: number [] = [1,2,3,4,5,67,6]
+console.log(nums)
+
+//ERR
+// const nums: number [] = [1,2,3,4,5,67,6 , "Asjad", true]
+// console.log(nums)
+
+
+const str: string [] = ["Asjad","Hello"]
+console.log(str)
+
+//ERR
+// const str: string [] = [1,2,3,4,5,67,6 , "Asjad", true]
+// console.log(str)
+
+
+const items:string [] = []
+console.log(items.push("Mouse"))
+
+//ERR
+// console.log(items.push(123))
+
+const numItem:number [] = []
+console.log(numItem.push(4123))
+
+//ERR
+// console.log(items.push("Mouse"))
+
+
+
+const singleDi : number [] = [1,2,3,45,6]
+const multiDi: number[][] = [[1,23,34,35,5]]
+
+
