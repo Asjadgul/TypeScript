2023a62c44b2ed0d4cbddb07837725a330ae4aae
Asjad Gull <148690534+Asjadgul@users.noreply.github.com>
2025-03-11 09:31:54 +0000

    Type Any in TypeScript

---
+++ AnyType_in_TypeScript.ts
@@ -0,0 +1,8 @@
+// Any Type
+// Any type is used when we deal with third-party programs
+//  and expect any variable but we don't know the exact type
+//  of variable
+
+let any: any = "String"
+ any = 0
+ any = true
