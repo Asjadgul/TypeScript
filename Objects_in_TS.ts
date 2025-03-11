2eb3bf6b9f04c9b42f2844cf346e5d328ad317cb
Asjad Gull <148690534+Asjadgul@users.noreply.github.com>
2025-03-11 09:41:30 +0000

    Objects in TS

---
+++ Objects_in_TS.ts
@@ -0,0 +1,7 @@
+const person: {firstName: string; lastName: string; age: number} = {
+    firstName : "Asjad",
+    lastName: "Gull",
+    age: 40
+}
+
+console.log(`First Name: ${person.firstName} ,  Last Name: ${person.lastName}, Age: ${person.age}`)
