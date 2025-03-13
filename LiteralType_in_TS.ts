5efeb0cde36151d7a4eb7491391d175937f1c266
Asjad Gull <148690534+Asjadgul@users.noreply.github.com>
2025-03-13 07:43:14 +0000

    Literals Type in TS

---
+++ LiteralType_in_TS.ts
@@ -0,0 +1,16 @@
+let color: "pink" | "red" | "yellow"
+
+
+// ERR
+// color = "orange"
+
+color = "pink"
+console.log(color)
+
+let isTrue : true | false
+isTrue = true
+console.log(isTrue)
+
+let password1: "secretPassword" = "secretPassword"
+
+console.log(password1)
